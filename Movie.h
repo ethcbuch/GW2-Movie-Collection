dbe3a8632e4da991bf1524c664fac1794fe05f94
Ethan Buchanan <ethcbuch@ut.utm.edu>
2022-10-29 10:43:51 -0500

    Finished makefile

---
+++ Movie.h
@@ -0,0 +1,13 @@
+#ifndef MOVIE_H
+#define MOVIE_H
+
+struct movie {
+  std::string movieTitle;
+  std::string directorName;
+  std::string movieRuntime;
+  std::string movieFormat;
+  std::string moviePrice;
+  std::string releaseYear;
+};
+
+#endif //MOVIE_H
